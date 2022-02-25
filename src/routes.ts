e6c51a6adac924308dd3d1fa50f9667c7efc3069
Bruno Weber <contato@brunoweber.com.br>
2022-02-25 02:54:29 -0300

    build(modules): add use cases folders and files

--- src/routes.ts
+++ src/routes.ts
@@ -3,20 +3,43 @@ import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 
 const routes = Router()
 
-routes.get("/quotes/list", (request: Request, response: Response) => response.json({
-  message: `List all freight cotes`
+/* Alias */
+routes.get("/", (request: Request, response: Response) => response.redirect("/quotations/create"))
+
+routes.get("/quotations/listAll", (request: Request, response: Response) => response.json({
+  message: `List all deliveries quotations`
 }))
 
-routes.post("/quotes/create", (request: Request, response: Response) => response.json({
-  message: "Create a new freight cote"
+routes.get("/quotations/listAllOpen", (request: Request, response: Response) => response.json({
+  message: `List all open deliveries quotations`
 }))
 
-routes.put("/quotes/update/:id", (request: Request, response: Response) => response.json({
-  message: `Update freight cote ${request.params.id}`
+routes.get("/quotations/listAllClosed", (request: Request, response: Response) => response.json({
+  message: `List all closesd deliveries quotations`
 }))
 
-routes.delete("/quotes/delete/:id", (request: Request, response: Response) => response.json({
-  message: `Delete a freight cote ${request.params.id}`
+routes.get("/quotations/show/:id", (request: Request, response: Response) => response.json({
+  message: `Show delivery cotation ${request.params.id}`
+}))
+
+routes.post("/quotations/create", (request: Request, response: Response) => response.json({
+  message: "Create a new delivery quotation"
+}))
+
+routes.put("/quotations/update/:id", (request: Request, response: Response) => response.json({
+  message: `Update delivery quotation ${request.params.id}`
+}))
+
+routes.patch("/quotations/update/:id/close", (request: Request, response: Response) => response.json({
+  message: `Update delivery quotation ${request.params.id} to closed`
+}))
+
+routes.patch("/quotations/update/:id/open", (request: Request, response: Response) => response.json({
+  message: `Update delivery quotation ${request.params.id} to open`
+}))
+
+routes.delete("/quotations/delete/:id", (request: Request, response: Response) => response.json({
+  message: `Delete a freight quotation ${request.params.id}`
 }))
 
 export { routes };
