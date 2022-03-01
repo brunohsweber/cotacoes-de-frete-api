af3217fc92098ad134da285a53369e59206b2244
Bruno Weber <contato@brunoweber.com.br>
2022-03-01 17:16:55 -0300

    feat(accounts): add authenticate user

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,17 @@
 import { Request, Response, Router } from "express"
+import { AuthenticateUserController } from "./modules/accounts/useCases/authenticateUser/AuthenticateUserController";
 import { CreateUserController } from "./modules/users/useCases/createUser/CreateUserController";
 
 const routes = Router()
 
 const createUserController = new CreateUserController()
+const authenticateUserController = new AuthenticateUserController()
 
 /* Alias */
 routes.get("/", (request: Request, response: Response) => response.json({ message: "Hello World!!" }))
 
 // Users
-routes.post("/user", createUserController.handle)
+routes.post("/users", createUserController.handle)
+routes.post("/users/authenticate", authenticateUserController.handle)
 
 export { routes };
